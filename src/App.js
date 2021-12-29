8de6e03d37ba6e6adc6d2daf1ec0fba7d81bfbb5
Manish Kumar <parjapat.mk@gmail.com>
2021-12-29 22:34:04 +0530

    ADDED: quiz project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef, useState} from "react";
+import {QUIZ_QUESTIONS} from "./quiz";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialState = {quesIndex: 0, value: '', showTable: false};
+    const initialTime = 20;
+    const quesTimerInterval = useRef(null);
+    const [quiz, setQuiz] = useState(QUIZ_QUESTIONS);
+    const [state, setState] = useState(initialState);
+    const [quesTimer, setQuesTimer] = useState(initialTime);
+
+    useEffect(() => {
+        handleQuesTimer();
+        return () => {
+            clearInterval(quesTimerInterval.current);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (quesTimer === 0) {
+            handleSkip();
+        }
+    }, [quesTimer]);
+
+    const handleQuesTimer = () => {
+        quesTimerInterval.current = setInterval(() => {
+            setQuesTimer((prevQuesTimer) => prevQuesTimer - 1);
+        }, 1000);
+    }
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        quiz[state.quesIndex].submittedAns = state.value;
+        handleQuiz();
+    }
+
+    const handleSkip = () => {
+        quiz[state.quesIndex].submittedAns = "Not Answered";
+        handleQuiz();
+    }
+
+    const handleQuiz = () => {
+        clearInterval(quesTimerInterval.current);
+        setQuiz([...quiz]);
+        if (!!quiz[state.quesIndex + 1]) {
+            setState({...state, quesIndex: state.quesIndex + 1, value: ''});
+            setQuesTimer(initialTime);
+            handleQuesTimer();
+        } else {
+            setState({...state, showTable: true});
+        }
+    }
+
+    return (
+        <div className="App">
+            <section className="App-header">
+                {!state.showTable && <form className="quiz-form" onSubmit={handleOnSubmit}>
+                    <p>Time left to submit the answer: {quesTimer} Sec</p>
+                    <h1 className="quiz-question">{quiz[state.quesIndex].question}</h1>
+                    <br/>
+                    {quiz[state.quesIndex].options.map((opt, index) =>
+                        <div className="text-left" key={index + 1}>
+                            <label>
+                                <input type="radio" value={opt} name="options" required
+                                       checked={state.value === opt}
+                                       onChange={() => setState({...state, value: opt})}
+                                />
+                                &nbsp; {opt}
+                            </label>
+                            <br/><br/>
+                        </div>)}
+                    <br/><br/>
+                    <button className="btn" type="submit">Submit Answer</button>
+                    <button className="btn ml-10px" type="button" onClick={handleSkip}>Skip Question</button>
+                </form>}
+
+                {!!state.showTable && <table>
+                    <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Question</th>
+                        <th>Correct Ans</th>
+                        <th>Submitted Ans</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {quiz.map((ques, index) =>
+                        <tr key={index + 1}>
+                            <td>{index + 1}</td>
+                            <td>{ques.question}</td>
+                            <td>{ques.answer}</td>
+                            <td>{ques.submittedAns}</td>
+                        </tr>)}
+                    </tbody>
+                </table>}
+            </section>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/quiz.js
@@ -0,0 +1,26 @@
+export const QUIZ_QUESTIONS = [
+    {
+        question: 'The song \'Veere\' is from my movie Udta Punjab. Yes or No?',
+        options: ['Yes', 'No'],
+        answer: 'No',
+        submittedAns: '',
+    },
+    {
+        question: 'Dil ______ Do! Complete the title of the song from my film ZNMD.',
+        options: ['Dhadakne', 'Dekhne'],
+        answer: 'Dhadakne',
+        submittedAns: '',
+    },
+    {
+        question: 'What role did I play in my movie 3 idiots?',
+        options: ['Teacher', 'Doctor'],
+        answer: 'Doctor',
+        submittedAns: '',
+    },
+    {
+        question: 'What is my job in Munna Michael?',
+        options: ['Street Dancer', 'Bartender'],
+        answer: 'Street Dancer',
+        submittedAns: '',
+    }
+]
